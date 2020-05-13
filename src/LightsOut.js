d820edc1cfd064f69c9a15c2c419b1d8fb2902da
Kevin Dolch <kevin@getvenga.com>
2020-05-13 18:30:37 -0400

    grid displaying and reset working

--- src/LightsOut.js
+++ src/LightsOut.js
@@ -12,10 +12,26 @@ export default class LightsOut extends React.Component {
       lights: this.constructor.populateGrid(),
       moveCount: 0
     }
+    this.handleReset = this.handleReset.bind(this);
   }
+
+  handleReset(e) {
+    e.preventDefault();
+    this.setState({ lights: this.constructor.populateGrid(), moveCount: 0 });
+  }
+
   render() {
+    const grid = this.state.lights.map((row, i) => (
+      <div className="row" key={i}>{row.map((cell, j) => (
+        <div className={`cell ${cell ? "on" : "off"}`} data-row={i} data-col={j} key={`${i}-${j}`} />
+      ))}
+      </div>))
+
     return (
-      <div className="App">
+      <div>
+        <div className="counter">{this.state.moveCount}</div>
+        {grid}
+        <button onClick={this.handleReset}>Reset</button>
       </div>
     );
   }
