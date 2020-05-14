8241a197d550f5df8951c0ae9d912b36d929a5fe
Kevin Dolch <kevin@getvenga.com>
2020-05-13 20:39:12 -0400

    win condition working correctly

--- src/LightsOut.js
+++ src/LightsOut.js
@@ -10,37 +10,50 @@ export default class LightsOut extends React.Component {
     super(props);
     this.state = {
       lights: this.constructor.populateGrid(),
-      moveCount: 0
+      moveCount: 0,
+      activeCells: 0
     }
+    this.state.activeCells = this.state.lights.reduce((total, row) => (row.reduce((subtotal, cell) => (cell + subtotal), 0) + total), 0);
     this.handleReset = this.handleReset.bind(this);
     this.handleToggle = this.handleToggle.bind(this);
   }
 
   handleReset(e) {
     e.preventDefault();
-    this.setState({ lights: this.constructor.populateGrid(), moveCount: 0 });
+    const lights = this.constructor.populateGrid();
+    const activeCells = lights.reduce((total, row) => (row.reduce((subtotal, cell) => (cell + subtotal), 0) + total), 0);
+    this.setState({ lights, activeCells, moveCount: 0 });
   }
 
   handleToggle(e) {
     e.preventDefault();
+    if(this.state.activeCells === 0) {
+      return;
+    }
     const row = parseInt(e.target.dataset.row, 10);
     const col = parseInt(e.target.dataset.col, 10);
     this.setState(prevState => {
       const dupLights = prevState.lights.map(r => r.slice())
+      let activeChange = 0
       dupLights[row][col] = dupLights[row][col] === 0 ? 1 : 0
+      activeChange += dupLights[row][col] === 0 ? -1 : 1
       if(row > 0) {
         dupLights[row-1][col] = dupLights[row-1][col] === 0 ? 1 : 0
+        activeChange += dupLights[row-1][col] === 0 ? -1 : 1
       }
       if (row < dupLights.length -1) {
         dupLights[row+1][col] = dupLights[row+1][col] === 0 ? 1 : 0
+        activeChange += dupLights[row+1][col] === 0 ? -1 : 1
       }
       if (col > 0){
         dupLights[row][col-1] = dupLights[row][col-1] === 0 ? 1 : 0
+        activeChange += dupLights[row][col-1] === 0 ? -1 : 1
       }
       if(col < dupLights[col].length -1) {
         dupLights[row][col+1] = dupLights[row][col+1] === 0 ? 1 : 0
+        activeChange += dupLights[row][col+1] === 0 ? -1 : 1
       }
-      return { lights: dupLights, moveCount: prevState.moveCount + 1 }
+      return { lights: dupLights, moveCount: prevState.moveCount + 1, activeCells: prevState.activeCells + activeChange }
     });
   }
 
@@ -55,6 +68,9 @@ export default class LightsOut extends React.Component {
       <div>
         <div className="counter">{this.state.moveCount}</div>
         {grid}
+        {this.state.activeCells === 0 &&
+          <div>Congratulations, you won in {this.state.moveCount} turns.  Click reset to play again!</div>
+        }
         <button onClick={this.handleReset}>Reset</button>
       </div>
     );
